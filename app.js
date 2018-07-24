09a63986368020955ae8520253517c31d3c48986
EmptyBrain <newell729@naver.com>
2018-07-25 00:39:56 +0900

    add html and css

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express'); /* 설치된 module은 require 명령어를 통해 불러 올 수 있고, 
                                     express module을 express 라는 이름의 변수로 저장하였습니다.*/
-
+var path = require('path');
 var app = express();    
 
-app.get('/',function(req, res) {  
-    res.send('Hello World!!!!!!!!!!!!!!!!!!!!!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(5000, function(){
     console.log('Server On!');
